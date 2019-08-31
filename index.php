5bdade66472ea4fa28a9b70c6aeec7a1b63fe7d2
Mbungu ngoma <hackmmn@gmail.com>
2019-08-31 05:51:43 +0100

    Add authentication process

--- index.php
+++ index.php
@@ -10,7 +10,11 @@ $sms = new OrangeSDK([
     'authorization_header' => 'Basic Q0R5WU5OeWdHc3RLZEdXQU40MFZ1NjZqT2QyQVBwOG86VTltTk15dlR4cG9OQVNFbw=='
 ]);
 
+print_r($sms->getToken());
+
+/*
 $sms->message('Code de verification 4')
     ->from('+243824109491')
     ->to('+2430971315850')
     ->send();
+    */
--- src/OrangeSDK.php
+++ src/OrangeSDK.php
@@ -2,9 +2,66 @@
 
 namespace Informagenie;
 
+use Curl\Curl;
+
 class OrangeSDK
 {
 
-    
+    public const BASE_URI = 'https://api.orange.com';
+
+    protected $credentials = [];
+
+    protected $token;
+
+    public $curl;
+
+
+    public function __construct(Array $credentials)
+    {
+        $this->credentials = $credentials;
+        $this->curl = new Curl(self::BASE_URI);
+    }
+
+    public function getToken(): String{
+
+        $authorizationHeader = $this->getAuthorizationHeader();
+
+        if(null === $authorizationHeader)
+        {
+            $authorizationHeader = 'Basic '. base64_encode($this->getClientId().':'. $this->getClientSecret());
+        }
+
+        $this->curl->setHeaders([
+            'Authorization' => $authorizationHeader,
+            'Content-Type' => 'application/x-www-form-urlencoded'
+        ]);
+        
+        if(empty($this->token))
+        {
+
+            $response = $this->curl->post('/oauth/v2/token', [
+                'grant_type' => 'client_credentials'
+                ]);
+
+            $this->token = $response->access_token;
+        }
+        
+        return $this->token;
+    }
+
+    protected function getClientId()
+    {
+        return $this->credentials['client_id'] ?? null;
+    }
+
+    protected function getClientSecret()
+    {
+        return $this->credentials['client_secret'] ?? null;
+    }
+
+    protected function getAuthorizationHeader()
+    {
+        return $this->credentials['authorization_header'] ?? null;
+    }
 
 }
