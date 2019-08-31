bde98edbb13ac9c988f218182ac361270477c8d0
Mbungu ngoma <hackmmn@gmail.com>
2019-08-31 05:16:52 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+use Informagenie\OrangeSDK;
+
+$sms = new OrangeSDK([
+    'client_id' => 'CDyYNNygGstKdGWAN40Vu66jOd2APp8o',
+    'client_secret' => 'U9mNMyvTxpoNASEo',
+    'authorization_header' => 'Basic Q0R5WU5OeWdHc3RLZEdXQU40MFZ1NjZqT2QyQVBwOG86VTltTk15dlR4cG9OQVNFbw=='
+]);
+
+$sms->message('Code de verification 4')
+    ->from('+243824109491')
+    ->to('+2430971315850')
+    ->send();
---
+++ src/OrangeSDK.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Informagenie;
+
+class OrangeSDK
+{
+
+    
+
+}
